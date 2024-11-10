70f0c30761e300c40b8c0a3bca15363d8bb0615f
Wasif479 <160731455+Wasif479@users.noreply.github.com>
2024-11-11 00:51:16 +0500

    Add files via upload

---
+++ milestone 3/script.js
@@ -0,0 +1,23 @@
+// Importing Form and Display Area for our Resume 
+var form = document.getElementById('dynamic-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+// Form Submission Handleing 
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevents reloading of the page 
+    //Collection of Input Values of the Form 
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //Generating Dynamic Resume Content 
+    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n\n    ");
+    // Displaying after content generation of the Dynamic Resume 
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('Display Element Missing !!');
+    }
+});
